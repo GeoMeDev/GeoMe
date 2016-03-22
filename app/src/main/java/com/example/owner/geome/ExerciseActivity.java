d3d80d38ad254375fdfeaae96098a706660da842
Nogah Goldberg <nogah.goldberg@idc.ac.il>
2016-03-22 22:32:33 +0200

    Added classes from core in parse. need to add a line for each class in main application and to make getters in each class thats in the core.

--- app/src/main/java/com/example/owner/geome/ExerciseActivity.java
+++ app/src/main/java/com/example/owner/geome/ExerciseActivity.java
@@ -1,16 +1,30 @@
 package com.example.owner.geome;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import static com.parse.ParseConfig.getInBackground;
 
 /**
  * Created by Nogah on 3/15/16.
@@ -30,16 +44,27 @@ public class ExerciseActivity extends AppCompatActivity {
 
         final TextView timerTV = (TextView) findViewById(R.id.exerciseActivity_timerTV);
 
+        final TextView question = (TextView) findViewById(R.id.exerciseActivity_question);
 
-        // Locate the class table named "ImageUpload" in Parse.com
-        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
-                "GeoSubject");
-
-        // Locate the objectId from the class
+        Log.d("yarkoni", "Making queries");
+        ParseQuery<GeoSubject> query = ParseQuery.getQuery("GeoSubjects");
         query.whereEqualTo("level", 1);
-        query.findInBackground();
+        query.findInBackground(new FindCallback<GeoSubject>() {
+            public void done(List<GeoSubject> geoSubjectList, ParseException e) {
+                if (e == null) {
+                    Log.d("yarkoni", "Retrieved " + geoSubjectList.size());
+                    Log.d("yarkoni", "Retrieved " + geoSubjectList.get(0).getObjectId());
 
-        getInBackground("svjq1hEE48",
+                    getGeoExercisesByGeoSubjectId(geoSubjectList.get(0).getObjectId());
+
+
+                } else {
+                    Log.d("yarkoni", "Error: " + e.getMessage());
+                }
+            }
+        });
+
+        /*getInBackground("svjq1hEE48",
                 new GetCallback<ParseObject>() {
 
                     public void done(ParseObject object,
@@ -72,10 +97,6 @@ public class ExerciseActivity extends AppCompatActivity {
                                             // Set the Bitmap into the
                                             // ImageView
                                             image.setImageBitmap(bmp);
-
-                                            // Close progress dialog
-                                            progressDialog.dismiss();
-
                                         } else {
                                             Log.d("test",
                                                     "There was a problem downloading the data.");
@@ -83,13 +104,12 @@ public class ExerciseActivity extends AppCompatActivity {
                                     }
                                 });
                     }
-                });
-    }
+                }); */
 
         new CountDownTimer(30000, 1000) {
 
             public void onTick(long millisUntilFinished) {
-                timerTV.setText(millisUntilFinished / 1000 +" sec" );
+                timerTV.setText(millisUntilFinished / 1000 + " sec");
             }
 
             public void onFinish() {
@@ -97,4 +117,28 @@ public class ExerciseActivity extends AppCompatActivity {
             }
         }.start();
     }
+
+    private void getGeoExercisesByGeoSubjectId(String objectId) {
+
+        ParseQuery<GeoExercise> query = ParseQuery.getQuery("GeoExercises");
+        query.whereEqualTo("subjectid", objectId);
+        query.findInBackground(new FindCallback<GeoExercise>() {
+            public void done(List<GeoExercise> geoExerciseList, ParseException e) {
+                if (e == null) {
+                    Log.d("yarkoni", "exercises size " + geoExerciseList.size());
+
+
+
+                } else {
+                    Log.d("yarkoni", "Error: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void getInBackground(String svjq1hEE48, GetCallback<ParseObject> getCallback) {
+
+    }
+
+
 }
